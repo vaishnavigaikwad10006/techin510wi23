2eac45637b2aac1d8bf1f611420e57a8da9ee490
Vaishnavi <vgaikwad@uw.edu>
2024-01-14 11:45:17 -0800

    first commit

---
+++ lab1/MainApp.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Vaishnavi Gaikwad")
+st.subheader("Product Designer")
+st.text("Adept at designing experiences that are rooted in user research and technically viable.")
+st.image('Logo VG.png')
